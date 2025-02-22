d6651baa2aa19037a5ed1a03e9e5054414849f47
Ayeshafatima123 <160357945+Ayeshafatima123@users.noreply.github.com>
2025-02-23 00:49:28 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,160 @@
+import React from 'react'
+import Image from 'next/image'
+function page() {
+  return (
+  
+             <div>       
+                    <div className='text-3xl flex justify-around'>
+                      SHOP
+                      On Sale
+                      New Arrival
+                      Brands
+                  </div>
+            
+                
+                  <div className='text-5xl'>
+                    FIND CLOTHES THAT MATCHES YOUR STYLE
+  
+                  <div className='text-white bg-black   flex justify-start  '>
+                    <button >
+                      Shop Now
+                    </button>
+                   </div>
+  
+                   < div className='contaniner'></div>
+                      < Image src="/Rectangle 2.jpg"width={2000}height={150}alt='Rectangle 2'></Image>
+
+                     
+                      <div  className='text-white bg-black flex justify-center'>
+                        VERSACE
+                       ZARA
+                        GUCCI
+                       PRADA                                     
+                        Calvinklein 
+                      </div>
+                    
+
+                      <div className='text-black font-bold flex justify-center'>
+                            NEW ARRIVALS
+                            </div>
+                             <div className='container'></div>
+                             <Image src="/image 7.jpg"width={150} height={50}alt='image 7'></Image>
+
+                              <h1 className='text-2xl'>
+                              T-shirt with Tape Details
+                              $120
+                              </h1>
+ 
+               <Image src='/image 8.jpg'width={150}height={55}alt='image 8'></Image>
+                            <h1 className='text-2xl'>
+                              Skinng Fit Jeanes
+                              $240
+                            </h1>
+
+                            <div className='container'></div>
+                            <Image src='/image 9.jpg'width={150}height={55}alt='image 9'></Image>
+
+                                    <h1 className='text-2xl  '>
+                                      Checkered Shirt 
+                                    $180
+                                      </h1>
+                                      
+                          <div className='container'></div>
+                           <Image src='/image 10.jpg'width={150}height={55}alt='image 10'></Image>
+                           <h1 className='text-2xl'>
+                            Sleeve Striped T.shirt
+                            $130
+
+                           </h1>
+
+                           
+                            <div className='text-black font-bold flex justify-center'>                                         
+                                   TOP SELLING
+                                   </div>
+
+                             <div className='container'></div>
+                             < Image src="/image 8 (1).jpg"width={150}height={55}alt='image 8(1)'></Image>
+                               
+                               <h1 className='text-2xl'>
+                                Courage Graphic T.shirt
+                                $145
+                               </h1>
+
+                               <div className='container'></div>
+                           <Image src="/image 9 (2).jpg"width={150}height={55}alt='image 9(2)'></Image>
+                              
+                                            
+                             <h1 className='text-2xl'>
+                             lossse Fit Bermuda Shorts
+                             $80
+                             </h1>
+                             <div className='text-black font-bold flex justify-center'>
+                              BROWER BY DRESS STYLES
+                             </div>
+
+                             <div className='container'></div>
+                             <h1 className='text-3xl flex justify-center'>
+                               Casual
+                             <Image src='/image 11 (4).jpg'width={300}height={55}alt='image 11(4)'></Image>
+                               </h1>
+
+                             <div className='container' ></div>
+                             <h1 className='text-3xl flex justify-center'>
+                             Formal
+                             <Image src="/image 13.jpg"width={300}height={60}alt='image 13 ' ></Image>
+                               </h1>
+
+
+                              <div className='container'></div>
+                              <h1 className='text-3xl flex justify-center'>
+                             Party 
+                              <Image src='/image 12 (2).jpg'width={300}height={60}alt='image 12 (2)'></Image>
+                                </h1>
+                                
+                                
+                              <div className='container'></div>
+                              <h1 className='text-3xl flex justify-center'>
+                              Gym
+                              <Image src="/image 14.jpg"width={300}height={60}alt='image 14'></Image>
+                              </h1>
+                              </div>
+
+                              <div  className="text-4xl font-bold">
+                                OUR HAPPY CUSTMERS
+            
+                              </div>
+          
+          
+                      
+                                                                                                                                                   
+
+                    
+                     
+                    </div>
+          
+            
+                
+          
+          
+  
+             
+              
+                    
+                        
+            
+                
+      
+          
+  
+  
+  
+  
+
+      
+
+  
+  )
+}
+
+export default page
+
